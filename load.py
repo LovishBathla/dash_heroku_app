9ae6b09967b40d54aa53eb336a7291402da8c4f6
Lovish Bathla <lovishbathla@rocketmail.com>
2019-09-14 14:38:21 +0530

    Update load.py

--- load.py
+++ load.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def load_data():
 
-    df = pd.read_csv('D:\dash-ece-app-master\ECE_copy.csv',low_memory=False)
+    df = pd.read_csv('ECE_copy.csv',low_memory=False)
 
     # Estandarizando el tipo de los datos
     for i in df.columns:
